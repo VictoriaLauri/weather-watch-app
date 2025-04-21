d383e6be6ba49c23919d549e5af70f5d810e7792
Sadie Jones <sadieannejones@icloud.com>
2025-04-21 18:39:42 +0100

    added to landing page folder

---
+++ client/src/pages/landingPage/App.js
@@ -0,0 +1,71 @@
+import "./App.css"
+import { WeatherDisplay } from "../../components/WeatherDisplay/WeatherDisplay"
+
+function App() {
+  
+  const intros = [
+    {
+      id: 1,
+      name: "Victoria",
+      movie: "Autumn in New York",
+      weather: "rainy",
+      food: "ice cream",
+    },
+    {
+      id: 2,
+      name: "Sadie",
+      movie: "Hercules",
+      weather: "raining",
+      food: "macaroni cheese",
+    },
+    {
+      id: 3,
+      name: "Kiki",
+      movie: "the one I watch next as I rarely watch movies twice",
+      weather: "sunny, cloudy, rainy, snowy, I love watching movies",
+      food: "olives, wine and kitty cuddles",
+    },
+    {
+      id: 4,
+      name: "Safa",
+      movie: "Pride & Prejudice",
+      weather: "rainy",
+      food: "hot chocolate"
+    },
+    {
+      id: 5,
+      name: "Alberta",
+      movie: "Moana",
+      weather: "sunny",
+      food: "fruit salad",
+    },
+  ]
+
+  return (
+    <div className="App">
+      <h2>Group-4-Fullstack Watch Party Picks</h2>
+      <WeatherDisplay />
+      <div className="watchParty">
+        {intros.map((intro) => (
+          <div className="blur">
+            <div key={intro.id} className="intro-card">
+              <h3>{intro.name}'s watch party:</h3>
+              <p>
+                My favorite movie is <strong>{intro.movie}.</strong>
+              </p>
+              <p>
+                Watch it when the weather is <strong>{intro.weather}.</strong>
+              </p>
+              <p>
+                Try some <strong>{intro.food}</strong> when watching it!
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <h2>Code First Girls Spring 2025</h2>
+    </div>
+  )
+}
+
+export default App
