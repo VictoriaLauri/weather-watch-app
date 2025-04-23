d3028bc731d4da1ff29ae9b495f4431fad0db386
kiki <kikivillarreal@gmail.com>
2025-04-23 22:40:28 +0100

    navbar

--- client/src/pages/landingPage/App.js
+++ client/src/pages/landingPage/App.js
@@ -1,13 +1,15 @@
+import React from 'react';
 import { UserProvider } from '../../components/context/UserContext'
 import { RecommendMovie } from '../../components/RecommendMovie/RecommendMovie'
 import { WeatherDisplay } from '../../components/WeatherDisplay/WeatherDisplay'
+import Navbar from '../../components/Navbar/Navbar'
 import './App.css'
 
 function App() {
   return (
     <UserProvider>
       <div className='App'>
-        <h2>WeatherWatch</h2>
+      <div><Navbar/></div>
         <div className='container'>
           <WeatherDisplay />
           <RecommendMovie />
