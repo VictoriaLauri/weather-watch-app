3b99aeec134a68b2cb1d9d5c3cf238bd9c34b6a7
VictoriaLauri <victoria@victorialauri.com>
2025-04-27 11:57:37 +0100

    moved app.js into root

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { UserProvider } from '../../components/context/UserContext'
+import { UserProvider } from './components/context/UserContext'
 // import { RecommendMovie } from '../../components/RecommendMovie/RecommendMovie'
-import BackgroundWrapper from '../../components/BackgroundWrapper/BackgroundWrapper'
-import Navbar from '../../components/Navbar/Navbar'
-import { WeatherDisplay } from '../../components/WeatherDisplay/WeatherDisplay'
+import BackgroundWrapper from './components/BackgroundWrapper/BackgroundWrapper'
+import Navbar from './components/Navbar/Navbar'
+import { WeatherDisplay } from './components/WeatherDisplay/WeatherDisplay'
 import './App.css'
 
 function App() {
