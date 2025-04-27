53c247f44f5e0888010a8fb42fbef2b0ccabe090
VictoriaLauri <victoria@victorialauri.com>
2025-04-27 18:08:43 +0100

    shared context now has movie fetch, localStorage for movie suggestion, movie info page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { UserProvider } from './components/context/UserContext'
 import Navbar from './components/Navbar/Navbar'
 import HomePageMovieSuggestion from './pages/HomePageMovieSuggestion/HomePageMovieSuggestion'
 import LandingPage from './pages/LandingPage/LandingPage'
+import MovieInformation from './pages/MovieInformation/MovieInformation'
 import SigningInPage from './pages/SigningInPage/SigningInPage'
 import SigningUpPage from './pages/SigningUpPage/SigningUpPage'
 
@@ -19,6 +20,7 @@ function App() {
           <Route path='/signin' element={<SigningInPage />} />
           <Route path='/signup' element={<SigningUpPage />} />
           <Route path='/watch' element={<HomePageMovieSuggestion />} />
+          <Route path='/movie' element={<MovieInformation />} />
         </Routes>
       </BackgroundWrapper>
     </UserProvider>
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,33 +1,40 @@
-//import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
-import logo from '../../assets/WW_logo_blue.png';
-import profileIcon from '../../assets/profile_icon.png';
-import './Navbar.css';
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/WW_logo_blue.png'
+import profileIcon from '../../assets/profile_icon.png'
+import { UserContext } from '../../components/context/UserContext'
+import './Navbar.css'
 
+function Navbar() {
+  const { logout } = useContext(UserContext)
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <div className='navbar-left'>
+            <Link to='/'>
+              <img src={logo} className='navbar-logo' alt='Logo' />
+            </Link>
+          </div>
 
-function Navbar(){
-
-    return (
-        <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <div className="navbar-left">
-                    <Link to="/">
-                    <img src={logo} className='navbar-logo' alt="Logo" />
-                    </Link>
-                    </div>
-
-                    <div>
-                        <div className="navbar-right"> 
-                        <Link to="./pages/profilePage">
-                        <img src={profileIcon} className='profile-icon' alt="Profile icon" />
-                        </Link>
-                        </div>
-                    </div>
-                </div>
-            </nav>
-        </>
-    )
+          <div>
+            <div className='navbar-right'>
+              <Link to='./pages/profilePage'>
+                <img
+                  src={profileIcon}
+                  className='profile-icon'
+                  alt='Profile icon'
+                />
+              </Link>
+              <button onClick={logout} className='logout-button'>
+                Logout
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
 }
 
-export default Navbar;
+export default Navbar
