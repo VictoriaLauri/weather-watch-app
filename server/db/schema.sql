3dc133b006edc638bf63997549057c474b3689dc
Sadie Jones <sadieannejones@icloud.com>
2025-04-30 20:54:00 +0100

    updated users db to include email and removed lon and lat

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -13,8 +13,7 @@ CREATE TABLE users (
   username VARCHAR(50) NOT NULL UNIQUE,
   password VARCHAR(255) NOT NULL,
   age INT NOT NULL,
-  latitude DECIMAL(9,6),
-  longitude DECIMAL(9,6),
+  email VARCHAR(255) NOT NULL UNIQUE,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
