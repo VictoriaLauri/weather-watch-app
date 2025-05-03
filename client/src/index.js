1adf4ab3fe9eaaf7ccb41e0a690d1562a772c34f
Sadie Jones <sadieannejones@icloud.com>
2025-05-03 21:23:59 +0100

    added userProvider

--- client/src/index.js
+++ client/src/index.js
@@ -9,11 +9,13 @@ import reportWebVitals from './reportWebVitals'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </BrowserRouter>
+    
+    <BrowserRouter> 
+    <UserProvider>
+    <App />
+    </UserProvider>
+    </BrowserRouter> 
+   
   </React.StrictMode>
 )
 
