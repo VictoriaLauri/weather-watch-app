adf1d9987b01706b90b5da46eefcd590851d841a
Sadie Jones <sadieannejones@icloud.com>
2025-05-03 14:44:09 +0100

    Aids in running modules and ensures setuptest is ran first

---
+++ client/src/config/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'], // This ensures that setupTests.js runs before your tests
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!some-package-to-transform)/", 
+    '/node_modules/(?!axios)/',
+  ],
+  moduleNameMapper: {
+    "^react-router-dom$": "<rootDir>/src/__mocks__/react-router-dom.js", 
+  },
+};
