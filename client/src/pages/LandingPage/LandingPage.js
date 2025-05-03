ccaeaac01158ef87b14dd27c4421d6e571aed4a5
Sadie Jones <sadieannejones@icloud.com>
2025-05-04 00:11:54 +0100

    Hide sign in and sign up when logged in. added suggestion button

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,25 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom' // Importing Link for navigation
+import { UserContext } from '../../components/context/UserContext'
 
 const LandingPage = () => {
+  const {token} = useContext(UserContext)
+
   return (
     <div className='landing-container'>
       <h1>Welcome to WeatherWatch</h1>
       <p>
-        Get personalized movie recommendations based on the weather at your
+        Get personalised movie recommendations based on the weather at your
         location.
       </p>
 
       <div className='button-container'>
+        {!token ?(
+          <>
         <Link to='/signup'>
           <button className='landing-button'>Sign Up</button>
         </Link>
         <Link to='/signin'>
           <button className='landing-button'>Sign In</button>
         </Link>
-        {/* <Link to='/watch'>
+        </> ):( 
+        <Link to='/watch'>
           <button className='landing-button'>Straight to Suggestions</button>
-        </Link> */}
+        </Link> 
+        )}
       </div>
     </div>
   )
