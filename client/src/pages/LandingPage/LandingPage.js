bc793130b1480fba1743a88f16c2dcad48e30ecc
Sadie Jones <sadieannejones@icloud.com>
2025-05-04 17:26:36 +0100

    Added classnames and kiki code

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,17 +1,28 @@
 import React, { useContext } from 'react'
 import { Link } from 'react-router-dom' // Importing Link for navigation
 import { UserContext } from '../../components/context/UserContext'
+import logoIcon from '../../assets/WW_logo_white.png';
+import skyBackground from '../../assets/sky-background.png';
+import cloudsBackground from '../../assets/clouds-background.png';
+import './LandingPage.css'
 
 const LandingPage = () => {
   const {token} = useContext(UserContext)
 
   return (
     <div className='landing-container'>
-      <h1>Welcome to WeatherWatch</h1>
-      <p>
-        Get personalised movie recommendations based on the weather at your
-        location.
-      </p>
+       <section className="top-section" style={{
+    background: `url(${skyBackground}) no-repeat center/cover`,}}>
+
+    <div className="logo-title">
+        <img src={logoIcon} alt="WeatherWatch logo" className="logo-icon" />
+      </div>
+
+      <p className="tagline">Let the weather set the scene.</p>
+
+      <p className="description">
+          Your perfect movie night starts with the sky above! Whether it's a rainy evening, a sunny afternoon, or a chilly winter night, WeatherWatch curates the best films to match the weather outside.
+        </p>
 
       <div className='button-container'>
         {!token ?(
@@ -28,6 +39,10 @@ const LandingPage = () => {
         </Link> 
         )}
       </div>
+      </section>
+      <section className="bottom-section" style={{
+    background: `url(${cloudsBackground}) no-repeat center/cover`,}}>
+      </section>
     </div>
   )
 }
