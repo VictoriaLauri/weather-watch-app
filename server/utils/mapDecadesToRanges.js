5761e835427fcf9317c739378c2af9dac5adc4b0
Sadie Jones <sadieannejones@icloud.com>
2025-05-04 13:16:38 +0100

    Changed titles of decades

--- server/utils/mapDecadesToRanges.js
+++ server/utils/mapDecadesToRanges.js
@@ -5,11 +5,11 @@ export const mapDecadesToRanges = (label) => {
       return { start: 1900, end: 1969 }
     case 'Retro 70s and 80s':
       return { start: 1970, end: 1989 }
-    case '90s and 2000s Throwback':
+    case 'Throwback 90s and 2000s':
       return { start: 1990, end: 2009 }
     case 'Modern 2010s':
       return { start: 2010, end: 2019 }
-    case '2020s Fresh Hits':
+    case 'Fresh Hits 2020s':
       return { start: 2020, end: 2025 }
     default:
       return null // Handle unknown labels
