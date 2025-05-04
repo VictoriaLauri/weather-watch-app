d9b4838a88e8198e9580cb13e80b409d92669a48
Sadie Jones <sadieannejones@icloud.com>
2025-05-04 17:49:59 +0100

    Added capital letters

--- client/src/components/WeatherDisplay/GetMoodFromWeather.jsx
+++ client/src/components/WeatherDisplay/GetMoodFromWeather.jsx
@@ -2,21 +2,21 @@
 
 function getMoodFromWeather(condition) {
   switch (condition) {
-    case 'clear': return 'funny'
-    case 'clouds': return 'intense'
+    case 'clear': return 'Funny'
+    case 'clouds': return 'Intense'
     case 'rain':
     case 'drizzle':
-      return 'emotional'
-    case 'thunderstorm': return 'thrilling'
-    case 'snow': return 'magical'
+      return 'Emotional'
+    case 'thunderstorm': return 'Thrilling'
+    case 'snow': return 'Magical'
     case 'mist':
     case 'fog':
-      return 'mysterious'
-    case 'haze': return 'thought-provoking'
+      return 'Mysterious'
+    case 'haze': return 'Thought-provoking'
     case 'smoke':
     case 'dust':
-      return 'epic'
-    default: return 'family-friendly'
+      return 'Epic'
+    default: return 'Family-friendly'
   }
 }
 
